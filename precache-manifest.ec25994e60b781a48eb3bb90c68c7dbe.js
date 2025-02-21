ddd745397a0d0d7d9abd162d99aa31ea1a51ac8b
Masum Raza <masumrazacsegmail.com>
2025-02-22 00:10:02 +0530

    Updates

---
+++ precache-manifest.ec25994e60b781a48eb3bb90c68c7dbe.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a337775703f13c5e4e9f7f0d934d23fb",
+    "url": "/jashneraza/index.html"
+  },
+  {
+    "revision": "56f0d90530a37b5b3d04",
+    "url": "/jashneraza/static/css/2.626c27f0.chunk.css"
+  },
+  {
+    "revision": "e7a1ecfeb3fa5587d670",
+    "url": "/jashneraza/static/css/main.51b07fb5.chunk.css"
+  },
+  {
+    "revision": "56f0d90530a37b5b3d04",
+    "url": "/jashneraza/static/js/2.ab45b34e.chunk.js"
+  },
+  {
+    "revision": "66ed659f9d3b068c99e858ba560c69f0",
+    "url": "/jashneraza/static/js/2.ab45b34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a1ecfeb3fa5587d670",
+    "url": "/jashneraza/static/js/main.acd058d0.chunk.js"
+  },
+  {
+    "revision": "1fefc56cf65cae8ca082",
+    "url": "/jashneraza/static/js/runtime-main.44ff428c.js"
+  },
+  {
+    "revision": "932dfad1fbea5b01da84f83460155beb",
+    "url": "/jashneraza/static/media/FR Society (250 x 75 px) (1).932dfad1.svg"
+  },
+  {
+    "revision": "6619affa17554cbd9e671bacf4e2cdb9",
+    "url": "/jashneraza/static/media/aboutCollage.6619affa.png"
+  },
+  {
+    "revision": "204acebefcbeda89d85796e765c20eac",
+    "url": "/jashneraza/static/media/alam.204acebe.png"
+  },
+  {
+    "revision": "29dedd086c991743cc767660127d6c29",
+    "url": "/jashneraza/static/media/amin.29dedd08.png"
+  },
+  {
+    "revision": "6481a32b57e601dbfb2b6f2218121807",
+    "url": "/jashneraza/static/media/asmuddin.6481a32b.png"
+  },
+  {
+    "revision": "55de119e5b7a3eeddc42ed5d3f07186b",
+    "url": "/jashneraza/static/media/bablu-removebg-preview.55de119e.png"
+  },
+  {
+    "revision": "1fdf7f28b81dec4bab436b5e087ba250",
+    "url": "/jashneraza/static/media/faizaneraza.1fdf7f28.svg"
+  },
+  {
+    "revision": "607b4185332150826993afe2c10b8773",
+    "url": "/jashneraza/static/media/fr_news.607b4185.png"
+  },
+  {
+    "revision": "50225bcd8ab0ca1d6a721e254a05c89c",
+    "url": "/jashneraza/static/media/guddu.50225bcd.png"
+  },
+  {
+    "revision": "dd97345da7525a6c7ca53bae99bc8959",
+    "url": "/jashneraza/static/media/hero-image.dd97345d.jpg"
+  },
+  {
+    "revision": "3ead0b8d28c337921a44416fb7cf3b2a",
+    "url": "/jashneraza/static/media/islamuddin-removebg-preview.3ead0b8d.png"
+  },
+  {
+    "revision": "7a96cc788eb69701356dceffc2646c22",
+    "url": "/jashneraza/static/media/jashneraza.7a96cc78.svg"
+  },
+  {
+    "revision": "15a339d282a35378f355cc8e9d716f6c",
+    "url": "/jashneraza/static/media/kalameraza.15a339d2.svg"
+  },
+  {
+    "revision": "def3c6ee335cd5c69385260ab24b0948",
+    "url": "/jashneraza/static/media/kalim-removebg-preview.def3c6ee.png"
+  },
+  {
+    "revision": "db807f653b59c45537671352a1781b5c",
+    "url": "/jashneraza/static/media/newsjr.db807f65.svg"
+  },
+  {
+    "revision": "95946e9ed7f25c3976aa3cdaf0d50c54",
+    "url": "/jashneraza/static/media/profile-1.95946e9e.png"
+  },
+  {
+    "revision": "5f5c9758ed77ac1189180f083a6e9ba2",
+    "url": "/jashneraza/static/media/profile-2.5f5c9758.png"
+  },
+  {
+    "revision": "65e4f40e2d449b88cbbc7cfc31d3f733",
+    "url": "/jashneraza/static/media/profile-3.65e4f40e.png"
+  },
+  {
+    "revision": "f2b063dc91970c4ec6d751e6fd450f77",
+    "url": "/jashneraza/static/media/profile-4.f2b063dc.png"
+  },
+  {
+    "revision": "9dce7277629f75a65f7f924ce61177be",
+    "url": "/jashneraza/static/media/profile-5.9dce7277.png"
+  },
+  {
+    "revision": "3ec086f12b09e8d6014bce42d4b7a4a6",
+    "url": "/jashneraza/static/media/profile-6.3ec086f1.png"
+  },
+  {
+    "revision": "3012b9b5f6b2c53cc3675de4abf6b632",
+    "url": "/jashneraza/static/media/sabir-removebg-preview.3012b9b5.png"
+  },
+  {
+    "revision": "5d203bf33166a4c8c0161d0f3b9d4729",
+    "url": "/jashneraza/static/media/shifaeraza.5d203bf3.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/jashneraza/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/jashneraza/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/jashneraza/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/jashneraza/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8281b0608e0ea563fe51d4d8e8be6118",
+    "url": "/jashneraza/static/media/tauhid_Khalifa-removebg-preview.8281b060.png"
+  },
+  {
+    "revision": "b277708f040b80fd5a495f3553000f36",
+    "url": "/jashneraza/static/media/video.b277708f.mp4"
+  },
+  {
+    "revision": "8b5ab1752abc4a20a3987735c565848c",
+    "url": "/jashneraza/static/media/work4.8b5ab175.jpg"
+  },
+  {
+    "revision": "a89d24fcb597ef60eae443d39df9dc2e",
+    "url": "/jashneraza/static/media/work5.a89d24fc.jpg"
+  },
+  {
+    "revision": "240efe6a7bc335f035f9fe36b9fac0b3",
+    "url": "/jashneraza/static/media/work6.240efe6a.jpg"
+  },
+  {
+    "revision": "be2ceca0f860b1fdbbde2e8adc02d4e4",
+    "url": "/jashneraza/static/media/work7.be2ceca0.jpg"
+  },
+  {
+    "revision": "ee0fc4657b65bf19902cdc162bf20f0b",
+    "url": "/jashneraza/static/media/yawar-removebg-preview.ee0fc465.png"
+  }
+]);
